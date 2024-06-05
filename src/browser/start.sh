fc3469afb3b0c590cfdb6a7bf4a62b3a05110b1c
Jonathan Gregson <jdgregson@gmail.com>
2024-06-04 21:19:18 -0700

    Updates.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -6,8 +6,9 @@ podman run \
   --rm \
   --name jdgregson-browser \
   --shm-size=4g \
-  -e VNC_PW=password \
   -p 127.0.0.1:6901:6901 \
+  -e VNC_PW=password \
+  -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized --force-dark-mode" \
+  -e LAUNCH_URL="about:blank" \
   -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/chrome/policies/managed \
-  -e APP_ARGS="--no-default-browser-check --no-first-run" \
-  docker.io/kasmweb/chrome:1.14.0-rolling
+  docker.io/kasmweb/chrome:1.15.0-rolling
