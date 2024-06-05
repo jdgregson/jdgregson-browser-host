993c94ba7a8be734d9f5d7dd4a67e9759ec723f4
Jonathan Gregson <jdgregson@gmail.com>
2024-06-04 21:03:25 -0700

    Updates.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -9,4 +9,5 @@ podman run \
   -e VNC_PW=password \
   -p 127.0.0.1:6901:6901 \
   -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/chrome/policies/managed \
+  -e APP_ARGS="--no-default-browser-check --no-first-run" \
   docker.io/kasmweb/chrome:1.14.0-rolling
