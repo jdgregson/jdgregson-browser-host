73d5e075a901b1152635b7cf6bf75b70a3e19d4e
Jonathan Gregson <jdgregson@gmail.com>
2024-06-04 19:37:19 -0700

    Testing policies.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -8,4 +8,5 @@ podman run \
   --shm-size=4g \
   -e VNC_PW=password \
   -p 127.0.0.1:6901:6901 \
+  -v /opt/jdgregson-browser-host/browser/policies:/etc/opt/chrome/policies/managed \
   docker.io/kasmweb/chrome:1.14.0-rolling
