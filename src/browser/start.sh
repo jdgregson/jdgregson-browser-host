e5dadfccb7d9bc6e3880f04dfabb3f868cc58205
Jonathan Gregson <jdgregson@gmail.com>
2024-06-06 00:51:22 -0700

    Hard-coded user agent.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -22,7 +22,7 @@ podman run \
   --shm-size=4g \
   -p 127.0.0.1:6901:6901 \
   -e VNC_PW=$VNC_PASSWORD \
-  -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized" \
+  -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized --user-agent=\"Mozilla/5.0\ (iPhone;\ CPU\ iPhone\ OS\ 17_5_1\ like\ Mac\ OS\ X)\ AppleWebKit/605.1.15\ (KHTML,\ like\ Gecko)\ Version/17.4.1\ Mobile/15E148\ Safari/604.1\"" \
   -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/edge/policies/managed \
   -v /opt/jdgregson-browser-host/src/browser/Default:/home/kasm-default-profile/.config/microsoft-edge/Default \
   docker.io/kasmweb/$APP:1.15.0-rolling
