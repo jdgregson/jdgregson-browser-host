8e0aeb8d8941eee142343a84e81224f6735ba010
Jonathan Gregson <jdgregson@gmail.com>
2024-06-05 20:13:39 -0700

    Switched to Edge.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -2,6 +2,7 @@
 
 CONTAINER_NAME=jdgregson-browser-container
 VNC_PASSWORD=password
+APP=edge
 
 podman image prune -f
 podman rm $CONTAINER_NAME -f
@@ -13,7 +14,6 @@ podman run \
   -p 127.0.0.1:6901:6901 \
   -e VNC_PW=$VNC_PASSWORD \
   -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized" \
-  -e LAUNCH_URL="about:blank" \
-  -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/chrome/policies/managed \
-  -v /opt/jdgregson-browser-host/src/browser/Default:/home/kasm-default-profile/.config/google-chrome/Default \
-  docker.io/kasmweb/chrome:1.15.0-rolling
+  -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/edge/policies/managed \
+  -v /opt/jdgregson-browser-host/src/browser/Default:/home/kasm-default-profile/.config/microsoft-edge/Default \
+  docker.io/kasmweb/$APP:1.15.0-rolling
