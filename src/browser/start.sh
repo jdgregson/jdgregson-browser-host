6468b1cef9ed4e20e2a43257853d293dd03374b5
Jonathan Gregson <jdgregson@gmail.com>
2024-06-06 22:47:14 -0700

    Updates.

--- src/browser/start.sh
+++ src/browser/start.sh
@@ -22,8 +22,8 @@ podman run \
   --shm-size=4g \
   -p 127.0.0.1:6901:6901 \
   -e VNC_PW=$VNC_PASSWORD \
-  -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized --load-extension=/opt/extensions/UserAgent" \
-  -v /opt/jdgregson-browser-host/src/browser/extensions:/opt/extensions \
+  -e APP_ARGS="--no-default-browser-check --no-first-run --start-maximized --load-extension=/home/kasm-user/extensions/extension" \
+  -v /opt/jdgregson-browser-host/src/browser/extensions:/home/kasm-user/extensions \
   -v /opt/jdgregson-browser-host/src/browser/policies:/etc/opt/edge/policies/managed \
   -v /opt/jdgregson-browser-host/src/browser/Default:/home/kasm-default-profile/.config/microsoft-edge/Default \
   docker.io/kasmweb/$APP:1.15.0-rolling
